5cac88ee91bd46741af2da21ebab61888aa92352
waitholdthis <parker.tootill@gmail.com>
2026-05-14 08:00:20 -0400

    Bump PWA cache for walk-up songs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 (function initPwaInstall() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register(new URL("sw.js?v=37", window.location.href), { scope: "./" }).catch(() => {});
+      navigator.serviceWorker.register(new URL("sw.js?v=40", window.location.href), { scope: "./" }).catch(() => {});
     });
   }
 
